7f05fd308ff690b04b98ae442f0293f4610387a2
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-04-09 14:59:34 +0430

    Setup Navigation Component

--- app/src/main/java/ir/miare/androidcodechallenge/core/presentation/MainActivity.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/core/presentation/MainActivity.kt
@@ -2,10 +2,12 @@ package ir.miare.androidcodechallenge.core.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import ir.miare.androidcodechallenge.R
 import ir.miare.androidcodechallenge.RankingFragment
 import ir.miare.androidcodechallenge.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     var binding: ActivityMainBinding? = null
@@ -15,8 +17,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, RankingFragment(-1))
-            .commit()
+//        supportFragmentManager.beginTransaction()
+//            .add(R.id.fragmentContainer, RankingFragment(-1))
+//            .commit()
     }
 }
---
+++ app/src/main/java/ir/miare/androidcodechallenge/feature_top_scorers/presentation/TopScorersListFragment.kt
@@ -0,0 +1,24 @@
+package ir.miare.androidcodechallenge.feature_top_scorers.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import ir.miare.androidcodechallenge.R
+
+@AndroidEntryPoint
+class TopScorersListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top_scorers_list, container, false)
+    }
+}
