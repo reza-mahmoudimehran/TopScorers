13128cce3743cb95fb96bed0614642d9e0e5e1b0
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-04-09 14:28:19 +0430

    Transfer MainActivity to Presentation Layer

--- app/src/main/java/ir/miare/androidcodechallenge/core/presentation/MainActivity.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/core/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
-package ir.miare.androidcodechallenge
+package ir.miare.androidcodechallenge.core.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ir.miare.androidcodechallenge.R
+import ir.miare.androidcodechallenge.RankingFragment
 import ir.miare.androidcodechallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
