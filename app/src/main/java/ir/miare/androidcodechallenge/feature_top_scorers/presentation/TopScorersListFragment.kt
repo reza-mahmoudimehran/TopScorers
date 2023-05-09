68d27987f9e2f0b4a0c44200a5be0269dbf5b5e9
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-05-09 19:05:27 +0430

    Initial Setup for TopScorersListFragment

--- app/src/main/java/ir/miare/androidcodechallenge/feature_top_scorers/presentation/TopScorersListFragment.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/feature_top_scorers/presentation/TopScorersListFragment.kt
@@ -1,24 +1,46 @@
 package ir.miare.androidcodechallenge.feature_top_scorers.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import ir.miare.androidcodechallenge.R
+import ir.miare.androidcodechallenge.core.util.network.data
+import ir.miare.androidcodechallenge.databinding.FragmentTopScorersListBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TopScorersListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val viewModel: TopScorersListViewModel by viewModels()
+    private lateinit var binding: FragmentTopScorersListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_scorers_list, container, false)
+    ): View {
+        binding = FragmentTopScorersListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.getTopScorersList()
+        collectData()
+    }
+
+    private fun collectData() {
+        lifecycleScope.launch {
+            launch {
+                viewModel.topScorersList.collectLatest {
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/ir/miare/androidcodechallenge/feature_top_scorers/presentation/TopScorersListViewModel.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/feature_top_scorers/presentation/TopScorersListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.miare.androidcodechallenge.core.util.network.ApiResult
 import ir.miare.androidcodechallenge.feature_top_scorers.domain.entity.FakeData
+import ir.miare.androidcodechallenge.feature_top_scorers.domain.entity.TopScorersListItem
 import ir.miare.androidcodechallenge.feature_top_scorers.domain.usecase.GetTopScorersListUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,14 +17,14 @@ class TopScorersListViewModel @Inject constructor(
     private val getTopScorersListUseCase: GetTopScorersListUseCase
 ): ViewModel(){
 
-    private val _topScorersList = MutableStateFlow<ApiResult<List<FakeData>>>(ApiResult.Init)
-    val topScorersList: StateFlow<ApiResult<List<FakeData>>>
+    private val _topScorersList = MutableStateFlow<ApiResult<List<TopScorersListItem>>>(ApiResult.Init)
+    val topScorersList: StateFlow<ApiResult<List<TopScorersListItem>>>
         get() = _topScorersList
 
 
     fun getTopScorersList(){
         viewModelScope.launch {
-//            _topScorersList.value = getTopScorersListUseCase(Unit)
+            _topScorersList.value = getTopScorersListUseCase()
         }
     }
 }
