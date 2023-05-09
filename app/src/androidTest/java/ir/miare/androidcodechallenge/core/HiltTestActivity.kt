199f908d60d5863084d8be27ba9a6457913b21d7
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-05-10 01:57:40 +0430

    Setup Hilt Test Runner

---
+++ app/src/androidTest/java/ir/miare/androidcodechallenge/core/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package ir.miare.androidcodechallenge.core
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
