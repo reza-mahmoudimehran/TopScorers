44ee44b6a1b2b907a5d073e589c44322ffa23848
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-05-10 04:31:04 +0430

    Write UI Tests for All Sort Types

--- app/src/androidTest/java/ir/miare/androidcodechallenge/feature_top_scorers/presentation/TopScorersListFragmentTest.kt
+++ app/src/androidTest/java/ir/miare/androidcodechallenge/feature_top_scorers/presentation/TopScorersListFragmentTest.kt
@@ -2,6 +2,8 @@ package ir.miare.androidcodechallenge.feature_top_scorers.presentation
 
 import androidx.navigation.NavController
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -10,8 +12,7 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import ir.miare.androidcodechallenge.R
 import ir.miare.androidcodechallenge.core.presentation.MainActivity
-import ir.miare.androidcodechallenge.feature_top_scorers.domain.EXPECTED_ITEM_SORTED_BY_NONE
-import ir.miare.androidcodechallenge.feature_top_scorers.domain.TEST_ITEM_INDEX
+import ir.miare.androidcodechallenge.feature_top_scorers.domain.*
 import ir.miare.androidcodechallenge.feature_top_scorers.util.atPositionOnView
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
@@ -62,4 +63,64 @@ class TopScorersListFragmentTest {
             )
     }
 
+    @Test
+    fun selectRankSortType_shouldDisplayExpectedItemSortedByRank_atTextItemIndex() {
+        onView(withId(R.id.rdb_sort_by_ranking)).perform(click())
+
+        onView(withId(R.id.rcv_league))
+            .perform(
+                RecyclerViewActions.scrollToPosition<TopScorersAdapter.PlayerViewHolder>(
+                    TEST_ITEM_INDEX
+                )
+            )
+            .check(
+                matches(
+                    atPositionOnView(
+                        TEST_ITEM_INDEX,
+                        withText(EXPECTED_ITEM_SORTED_BY_RANK.player.name),
+                        R.id.player_name
+                    )
+                )
+            )
+    }
+    @Test
+    fun selectAverageSortType_shouldDisplayExpectedItemSortedByAverage_atTextItemIndex() {
+        onView(withId(R.id.rdb_sort_by_average)).perform(click())
+
+        onView(withId(R.id.rcv_league))
+            .perform(
+                RecyclerViewActions.scrollToPosition<TopScorersAdapter.PlayerViewHolder>(
+                    TEST_ITEM_INDEX
+                )
+            )
+            .check(
+                matches(
+                    atPositionOnView(
+                        TEST_ITEM_INDEX,
+                        withText(EXPECTED_ITEM_SORTED_BY_AVERAGE.player.name),
+                        R.id.player_name
+                    )
+                )
+            )
+    }
+    @Test
+    fun selectMostGoalSortType_shouldDisplayExpectedItemSortedByMostGoal_atTextItemIndex() {
+        onView(withId(R.id.rdb_sort_by_most_goal)).perform(click())
+
+        onView(withId(R.id.rcv_league))
+            .perform(
+                RecyclerViewActions.scrollToPosition<TopScorersAdapter.PlayerViewHolder>(
+                    TEST_ITEM_INDEX
+                )
+            )
+            .check(
+                matches(
+                    atPositionOnView(
+                        TEST_ITEM_INDEX,
+                        withText(EXPECTED_ITEM_SORTED_BY_MOST_GOAL.player.name),
+                        R.id.player_name
+                    )
+                )
+            )
+    }
 }
